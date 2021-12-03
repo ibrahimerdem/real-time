75b1e14e063812124de7cafe28e75b386e0650ac
ibrahimerdem <ibrahimerdemkalkan@gmail.com>
2021-12-03 14:01:55 +0300

    wait point

--- RealTimeApp/frontPage/consumer.py
+++ RealTimeApp/frontPage/consumer.py
@@ -8,6 +8,16 @@ import json
 class ChatConsumer(AsyncConsumer):
     async def websocket_connect(self, event):
         print('connected', event)
+        await self.send({
+            'type': 'websocket.accept'
+        })
+        other_user = self.scope['url_route']['kwargs']['username']
+        me = self.scope['user']
+        print(other_user, me)
+        await self.send({
+            'type': 'websocket.send',
+            'text': 'hello from server'
+        })
 
     async def websocket_receive(self, event):
         print('receive', event)
